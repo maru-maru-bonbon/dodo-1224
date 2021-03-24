79e762ceac767eae475a76a17d1e1abc932ceab1
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-03-25 03:15:06 +0900

    userモデルを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        has_many :plans
+        has_many :participants
+        has_many :participant_users, through: :participants, source: :plan
 end

53466f48fbbf90c5dc2fcd451acd41423f20c162
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-03-31 12:50:21 +0900

    privateメソッド内にデータ送信を認めるカラムを設定

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,30 @@
 class PlansController < ApplicationController
 
+
   def index
     @plans =Plan.all.order("created_at DESC")
   end
 
+
+
+
+
+
+
+
+  private
+
+  def plan_params
+    params.require(:message).permit(
+      :image,
+      :name,
+      :description,
+      :item,
+      :category_id,
+      :term_id,
+      :price_id).
+      merge(user_id: current_user.id)
+  end
+
+ 
 end

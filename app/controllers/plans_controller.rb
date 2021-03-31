762fb525c7b6934a352e6187f50b3a50c28b06eb
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-03-31 22:06:05 +0900

    ログインしていない状態でindex showのページ以外に入った際にトップページに戻されるように編集

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,5 @@
 class PlansController < ApplicationController
+  before_action :move_to_index, except:  [:index, :show]
 
 
   def index
@@ -37,5 +38,11 @@ class PlansController < ApplicationController
       merge(user_id: current_user.id)
   end
 
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index     
+    end
+  end
  
 end

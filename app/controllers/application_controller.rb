afa3a6c3cda41355b5a17166776b9abb5a8f31bc
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-04-01 13:04:31 +0900

    apptication_controllerファイルにdevieのnicknameデータを許可する様に編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :devise_parameter_sanitizers, if: :devise_controller?
+
+  private
+
+  def devise_parameter_sanitizers
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end

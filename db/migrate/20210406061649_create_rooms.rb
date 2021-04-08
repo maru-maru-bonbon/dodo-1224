e1ab81ee7fc241b697eea4f59bc6f49ae1bc4b9a
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-04-08 23:57:51 +0900

    roomモデル作成

---
+++ db/migrate/20210406061649_create_rooms.rb
@@ -0,0 +1,12 @@
+class CreateRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rooms do |t|
+      t.string :room_name
+      t.integer :member
+      t.references :plan, null: false, foreign_key: true
+
+
+      t.timestamps
+    end
+  end
+end

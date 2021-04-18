5823cfc50c05699a7c167848839fe4c84556e52e
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-04-18 17:22:03 +0900

    roomコントローラー編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,4 @@
 class RoomsController < ApplicationController
-  def index
-    @user = current_user
-    @currentRoomUser = current_user.room_users
-    myRoomIds = []
-    @currentRoomUser.each do |room_users|
-      myRoomIds << room_users.room.id
-    end
-    @anotherRoomUser = RoomUser.where(room_id: myRoomIds).where.not(user_id: @user.id).order(created_at: :desc)
-  end
 
   def show
     @room = Room.find(params[:id])
@@ -17,13 +8,6 @@ class RoomsController < ApplicationController
     else
       redirect_back(fallback_location: root_path)
     end
-  end
-
-  def create
-    @room = Room.create(:name => "DM")
-    @entry1 = RoomUser.create(:room_id => @room.id, :user_id => current_user.id)
-    @entry2 = RoomUser.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
-    redirect_to room_path(@room.id)
   end
 
   def destroy

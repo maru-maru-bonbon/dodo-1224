0c9c52533c5d1894cfba8da442cee04682b2bf7c
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-04-19 11:06:56 +0900

    migrate/room_usersを編集

--- db/migrate/20210406062232_create_room_users.rb
+++ db/migrate/20210406062232_create_room_users.rb
@@ -1,8 +1,8 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :plan_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
 
       t.timestamps
     end
